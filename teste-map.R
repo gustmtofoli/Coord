e5c35e0e85ec92d5355a22dfd138df780688b811
Gustavo Tófoli <gustavo.tofoli@objective.com.br>
2018-05-02 18:19:57 -0300

    map and DT

---
+++ teste-map.R
@@ -0,0 +1,72 @@
+# install.packages("ggmap")
+# library(ggmap)
+# 
+brazil <- data.frame(lat = c(-6.081689, 5.826789, 6.569828, 9.443383, 3.583828),
+                  lon = c(145.3919, 144.2959, 146.7262, 147.2200, 143.6692))
+# 
+# map <- get_map(location = 'Brazil', zoom = 4)
+# 
+# mapPoints <- ggmap(map) + 
+#   geom_point(aes(x = long, y = lat), data = sp1, alpha = .5)
+# 
+# mapPoints
+
+install.packages("dismo")
+install.packages("raster")
+install.packages("rgdal")
+install.packages("rJava")
+install.packages("maptools")
+library(dismo)
+library(maptools)
+
+# file <- paste(system.file(package="dismo"), "/home/gustavo.tofoli/Área de Trabalho/bradypus.csv", sep="")
+
+data("wrld_simpl")
+sp <- read.table("/home/gustavo.tofoli/Área de Trabalho/sp.csv",  header=TRUE,  sep=",")
+grid <- read.table("/home/gustavo.tofoli/Área de Trabalho/grid.csv",  header=TRUE,  sep=",")
+
+sps <- unique(file$sp)
+plot(wrld_simpl, xlim=c(-80,70), ylim=c(-60,10), axes=TRUE, col="light yellow")
+box()
+
+for (sp in sort(sps)) {
+  sp <- subset(file, file$sp == sps[sp])[,2:3]
+  points(sp$lon, sp$lat, col=c('blue'), pch=20, cex=0.75)
+  # legend(x='right', legend=rev(sps),
+         # box.lty=0, fill=rev(c("red", "blue")),cex=.8, ncol=1)
+}
+
+# add pints
+# points(teste$lon, teste$lat, col='blue', pch=20, cex=0.75)
+
+# result <- c()
+diam <- 5
+r <- diam/2
+# coord_sp <- file[,2:3]
+# condition <- coord_sp$lon <= brazil$long + r & coord_sp$lon >= brazil$long - r & 
+#   coord_sp$lat <= brazil$lat + r & coord_sp$lat >= brazil$lat - r
+# file
+
+# for (coord_sp in sp[,2:3]) {
+#   print(coord_sp[1])
+#   for (coord_brazil in brazil) {
+#     print(coord_sp[1] <= coord_brazil[1])
+#   }
+# }
+
+condition <- sp$lon <= grid$lon + r & sp$lon >= grid$lon - r &
+             sp$lat <= grid$lat + r & sp$lat >= grid$lat - r
+
+teste <- c()
+teste_df <- data.frame()
+for (specie in levels(unique(sp$sp))) {
+  print(specie)
+  teste <- c(teste, specie)
+}
+teste_df <- data.frame(teste)
+
+teste
+teste_df
+grid
+
+
--- ui.R
+++ ui.R
@@ -1,31 +1,156 @@
 library(shiny)
+library(leaflet)
+library(DT)
 
 shinyUI(fluidPage(
   # titlePanel("Old Faithful Geyser Data"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput('file1', 'Choose file to upload',
-                accept = c(
-                  'text/csv',
-                  'text/comma-separated-values',
-                  'text/tab-separated-values',
-                  'text/plain',
-                  '.csv',
-                  '.tsv'
-                )
-      ),
-      tags$hr(),
-      checkboxInput('header', 'Header', TRUE),
-      radioButtons('sep', 'Separator',
-                   c(Comma=',',
-                     Semicolon=';',
-                     Tab='\t'),
-                   ','),
-      tags$hr()
-    ),
-    
-    mainPanel(
-      tableOutput("contents")
-    )
+  # sidebarLayout(
+  #   sidebarPanel(
+  #     fileInput('file1', 'Choose file to upload',
+  #               accept = c(
+  #                 'text/csv',
+  #                 'text/comma-separated-values',
+  #                 'text/tab-separated-values',
+  #                 'text/plain',
+  #                 '.csv',
+  #                 '.tsv'
+  #               )
+  #     ),
+  #     fileInput('file2', 'Choose file to upload',
+  #               accept = c(
+  #                 'text/csv',
+  #                 'text/comma-separated-values',
+  #                 'text/tab-separated-values',
+  #                 'text/plain',
+  #                 '.csv',
+  #                 '.tsv'
+  #               )
+  #     ),
+  #     tags$hr(),
+  #     checkboxInput('header', 'Header', TRUE),
+  #     radioButtons('sep', 'Separator',
+  #                  c(Comma=',',
+  #                    Semicolon=';',
+  #                    Tab='\t'),
+  #                  ','),
+  #     tags$hr()
+  #   ),
+  #   
+  #   
+  #   mainPanel(
+  #     tableOutput("contents_grid"),
+  #     tableOutput("contents_sp"),
+  #     tableOutput("contents_occ")
+  #   )
+  # )
+  navbarPage("Coord", id="nav",
+     tabPanel("Map",
+        verticalLayout(
+            sidebarPanel(
+                  fileInput('file1', 'Choose grid file to upload',
+                            accept = c(
+                              'text/csv',
+                              'text/comma-separated-values',
+                              'text/tab-separated-values',
+                              'text/plain',
+                              '.csv',
+                              '.tsv'
+                            )
+                  ),
+                  fileInput('file2', 'Choose sp file to upload',
+                            accept = c(
+                              'text/csv',
+                              'text/comma-separated-values',
+                              'text/tab-separated-values',
+                              'text/plain',
+                              '.csv',
+                              '.tsv'
+                            )
+                  ),
+                  tags$hr(),
+                  checkboxInput('header', 'Header', TRUE),
+                  radioButtons('sep', 'Separator',
+                               c(Comma=',',
+                                 Semicolon=';',
+                                 Tab='\t'),
+                               ','),
+                  tags$hr()
+              ),
+             tabsetPanel(
+               type = "tabs",
+               tabPanel("Grid", DT::dataTableOutput("grid")),
+               tabPanel("Sp", DT::dataTableOutput("sp")),
+               tabPanel("Map", leafletOutput("map", width="500", height="500")),
+               tabPanel("Result", DT::dataTableOutput("result")
+             )
+            # leafletOutput("map", width="500", height="500")
+        )
+          # div(class="outer",
+              
+              # tags$head(
+                # Include our custom CSS
+                # includeCSS("styles.css"),
+                # includeScript("gomap.js")
+              # ),
+              
+              # If not using custom CSS, set height of leafletOutput to a number instead of percent
+              # leafletOutput("map", width="500", height="500")
+              
+              # Shiny versions prior to 0.11 should use class = "modal" instead.
+              # absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+              #               draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+              #               width = 330, height = "auto",
+              #               
+              #               h2("ZIP explorer"),
+              #               
+              #               selectInput("color", "Color", vars),
+              #               selectInput("size", "Size", vars, selected = "adultpop"),
+              #               conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+              #                                # Only prompt for threshold when coloring or sizing by superzip
+              #                                numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+              #               ),
+              #               
+              #               plotOutput("histCentile", height = 200),
+              #               plotOutput("scatterCollegeIncome", height = 250)
+              # ),
+              
+              # tags$div(id="cite",
+              #          'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+              # )
+          # )
+     ),
+             
+             # tabPanel("Data explorer",
+             #          fluidRow(
+             #            column(3,
+             #                   selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+             #            ),
+             #            column(3,
+             #                   conditionalPanel("input.states",
+             #                                    selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+             #                   )
+             #            ),
+             #            column(3,
+             #                   conditionalPanel("input.states",
+             #                                    selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+             #                   )
+             #            )
+             #          ),
+             #          fluidRow(
+             #            column(1,
+             #                   numericInput("minScore", "Min score", min=0, max=100, value=0)
+             #            ),
+             #            column(1,
+             #                   numericInput("maxScore", "Max score", min=0, max=100, value=100)
+             #            )
+             #          ),
+             #          hr(),
+             #          DT::dataTableOutput("ziptable")
+             # ),
+             
+     conditionalPanel("false", icon("crosshair"))
   )
-))
+  
+  
+  
+)))
