57cde88e8dd7e744c89a6e1edc9c409d220d88f8
Gustavo <gmtofoli@gmail.com>
2018-11-17 02:53:20 -0200

    ensemble with sdm

--- dev-tests/ensemble_realistic_example.R
+++ dev-tests/ensemble_realistic_example.R
@@ -3,82 +3,127 @@ library(rgdal)
 library(sp)
 library(caret)
 library(sdm)
+library(raster)
+library(dismo)
 
-# le arquivo
-horn=read.csv("/home/gustavo/Documentos/SDMGISR-files/2_SDM Data/hornbill_my1.csv")
+# # le arquivo
+# horn=read.csv("/home/gustavo/Documentos/SDMGISR-files/2_SDM Data/hornbill_my1.csv")
+# 
+# # le arquivo 2
+# # pa=read.csv("/home/gustavo/Documentos/SDMGISR-files/2_SDM Data/Pres_abs.csv")
+# 
 
-# le arquivo 2
-# pa=read.csv("/home/gustavo/Documentos/SDMGISR-files/2_SDM Data/Pres_abs.csv")
+setwd("/home/gustavo/Documentos/SDMGISR-files/2_SDM Data/bioclim")
 
-library(spocc)
-spp = c("Buceros rhinoceros")
-buceros_rhinoceros = occ(spp, from = c('gbif'), gbifopts = list(hasCoordinate=TRUE))
-head(buceros_rhinoceros)
-data = occ2df(buceros_rhinoceros)
-data
-
-prs1= extract(stck, horn1)
-
-set.seed(1)
-
-backgr = randomPoints(stck, 500) #500 random points
-absvals = extract(stck, backgr) #choose absence values from the background
-pb = c(rep(1, nrow(prs1)), rep(0, nrow(absvals)))
-sdmdata = data.frame(cbind(pb, rbind(prs1, absvals)))
-
-head(sdmdata)
-
-sdmdata=na.omit(sdmdata)
-summary(sdmdata)
-
-tail(sdmdata)
-
-
-
-
-
-# buceros_rhinoceros_coord <- buceros_rhinoceros$gbif
-# b1 <- buceros_rhinoceros_coord$data
-# b2 <- b1$Buceros_rhinoceros
-# names(b2)
-
-
-# filtra apenas as informações referentes a espécie Buceros rhinoceros
-buceros_rhinoceros = subset(horn, species == 'Buceros rhinoceros')
-
-# retira a coluna do nome da espécie
-buceros_rhinoceros_coord = buceros_rhinoceros
-
-# training and testing
-trainIndex = createDataPartition(buceros_rhinoceros_coord$species, 
-                                 p = .75, 
-                                 list = FALSE) 
-
-training = buceros_rhinoceros_coord[ trainIndex,] 
-# nrow(training)
-testing= buceros_rhinoceros_coord[-trainIndex,] 
-# nrow(testing)
-
-training <- training[, 2:3]
-
-# converte o data frame training em um shapefile da classe SpatialPoints
-WGScoor <- training
-coordinates(WGScoor)=~long+lat
-proj4string(WGScoor)<- CRS("+proj=longlat +datum=WGS84")
-training_data <-spTransform(WGScoor,CRS("+proj=longlat"))
-
-# cria uma stack de predictors
+# cria uma stack de predictors ====================================================
 datafiles = Sys.glob("*.tif")
 datafiles #list of predictors
-
 stck = stack() #empty raster stack for storing raster layers
 for(i in 1:NROW(datafiles)){
   tempraster = raster(datafiles[i])
   stck = stack(stck,tempraster)
 }
+# =================================================================================
 
-d <- sdmData(formula=Ocurrence~., train=training_data, predictors=stck)
+
+# download das ocorrencias da espécie utilizando o GBIF ===========================
+library(spocc)
+spp = c("Buceros rhinoceros")
+buceros_rhinoceros = occ(spp, from = c('gbif'), gbifopts = list(hasCoordinate=TRUE))
+head(buceros_rhinoceros)
+data = occ2df(buceros_rhinoceros)
+# apenas lat e long
+data <- data[, 2:3]
+nrow(data)
+# =================================================================================
+
+
+# pegando as informações do predictors em cada ponto de ocorrência  ===============
+prs1 = extract(stck, data)
+prs1_df <- data.frame(prs1)
+prs1_df$long <- data$longitude
+prs1_df$lat <- data$latitude
+# =================================================================================
+
+
+# gerando pontos de ausências para o modelo =======================================
+set.seed(1)
+backgr = randomPoints(stck, 500) #500 random points
+absvals = extract(stck, backgr) #choose absence values from the background
+absvals_df <- data.frame()
+absvals_df$long <- backgr[, 'x']
+absvals_df$lat <- backgr[, 'y']
+pb = c(rep(1, nrow(prs1)), rep(0, nrow(absvals)))
+sdmdata = data.frame(cbind(pb, rbind(prs1_df, absvals_df)))
+head(sdmdata)
+sdmdata=na.omit(sdmdata)
+summary(sdmdata)
+# =================================================================================
+
+# separando conjuntos de treino e teste ===========================================
+# trainIndex = createDataPartition(sdmdata$pb, p = .75, list = FALSE) 
+# training = sdmdata[ trainIndex,] 
+# testing= sdmdata[-trainIndex,] 
+# =================================================================================
+
+
+# convertendo o data frame training em um shapefile da classe SpatialPoints =======
+WGScoor <- training
+coordinates(WGScoor)=~long+lat
+proj4string(WGScoor)<- CRS("+proj=longlat +datum=WGS84")
+training_data <-spTransform(WGScoor,CRS("+proj=longlat"))
+# =================================================================================
+
+
+# convertendo o data frame sdmData em um shapefile da classe SpatialPoints ========
+WGScoor2 <- sdmdata
+coordinates(WGScoor2)=~long+lat
+proj4string(WGScoor2)<- CRS("+proj=longlat +datum=WGS84")
+sdmData_shapefile <-spTransform(WGScoor2,CRS("+proj=longlat"))
+# =================================================================================
+
+
+# gerando sdmData =================================================================
+d <- sdmData(formula=pb~., train=sdmData_shapefile, predictors=stck)
 d
+# =================================================================================
 
-m <- sdm(species~.,data=d,methods=c('rf', 'fda','mars','svm'),
-         replicatin='boot',n=10)
+
+# modelo  =========================================================================
+m <- sdm(pb~.,data=d,methods=c('rf', 'fda','mars','svm'), replicatin='sub', 
+         test.percent = 25, n = 2)
+m
+getModelInfo(m)
+roc(m)
+roc(m,smooth=T)
+# =================================================================================
+
+
+# predict =========================================================================
+p1 <- predict(m, newdata = stck, filename = 'p1.img') 
+plot(p1)
+nlayers(p1)
+plot(p1[[1:2]])
+
+# média
+p1m <- predict(m, newdata = stck, filename = 'p2m.img', mean=T)
+plot(p1m)
+# =================================================================================
+
+
+# ensemble ========================================================================
+e1 <- ensemble(m, newdata = stck, filename = 'e1.img', 
+               setting = list(method = 'weighted', stat = 'AUC')) 
+e1
+plot(e1)
+
+e2 <- ensemble(m, newdata = stck, filename = 'e2.img',
+               setting=list(method = 'weighted', stat = 'TSS', opt = 2))
+e2
+plot(e2)
+
+e3 <- ensemble(m, newdata = stck, filename = 'e3.img', setting = list(method = 'unweighted'))
+plot(e3)
+
+installAll()
+gui(m)
