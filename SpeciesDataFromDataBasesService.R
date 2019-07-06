ce94c86b37fc11c4810b1e80f376572af0de65b8
Gustavo Tófoli <gustavotofoli@pop-os.localdomain>
2019-07-06 01:20:02 -0300

    little changes

--- SpeciesDataFromDataBasesService.R
+++ SpeciesDataFromDataBasesService.R
@@ -9,15 +9,33 @@ observeEvent(input$download_from_DB, {
       species_download <- input$sp_name
     }
     
-    showModal(modalDialog(
-      title = "Searching Data",
-      footer = NULL,
-      easyClose = FALSE
-      # paste0("Species: ", species_download),
-      # br(),
-      # paste0("Data base(s): ", input$select_data_bases)
       
+      showModal(modalDialog(
+        title = "Searching Data",
+        footer = NULL,
+        easyClose = FALSE
+  
+              
+        
+  
+        # paste0("Species: ", species_download),
+        # br(),
+      # paste0("Data base(s): ", input$select_data_bases)
+
     ))
+    
+    # value <- reactiveVal(0)
+    # 
+    # withProgress(message = 'Calculation in progress', value = 0,detail="0%", {
+    #   # run calculation
+    #   for (i in 1:10) {
+    #     Sys.sleep(0.5)
+    #     newValue <- value() + 1
+    #     value(newValue)
+    #     incProgress(1/10,detail = paste0(i*10,"%"))
+    #   }
+    #   Sys.sleep(0.5)
+    # })
     
     print("\n\n========================")
     print(input$select_data_bases)
@@ -48,6 +66,20 @@ observeEvent(input$download_from_DB, {
   }
 })
 
+
+
+
+
+
+
+
+observeEvent(input$download_from_DB, {
+  {
+    # This is certainly wrong:
+    output$console_text <- renderPrint(result <- myfunction(20))
+  }
+})
+
 observeEvent(input$see_db_with_records, {
   downloaded_species <- variables$sp_download_db
   data_bases = unique(downloaded_species$data_base)
@@ -69,7 +101,7 @@ observeEvent(input$see_downloaded_species, {
   print(unique(downloaded_species$data_base))
   print(df)
   showModal(modalDialog(
-    title = "Data Bases with records:",
+    title = "Species:",
     footer = NULL,
     easyClose = TRUE,
     renderDataTable(df, options = list(lengthChange = FALSE))
